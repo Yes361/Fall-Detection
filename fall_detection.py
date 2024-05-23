4cb9e339be1aaec0259ea85b2c50b852ab53ee1b
Raiyan Islam <Raiyan.islam2@student.tdsb.on.ca>
2024-05-23 12:33:43 -0400

    modified:   fall_detection.py

--- fall_detection.py
+++ fall_detection.py
@@ -9,9 +9,15 @@ import matplotlib.pyplot as plt
 
 def main(option, method, thr, title):
   video = cv2.VideoCapture(option)
+
+  # video.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+  # video.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
   initialize()
   begin_time = t.time()
   start = t.time()
+
+  fall_time = t.time()
   previous = [0] * (len(specific_points)+1)
   fall = False
   fall_detected = False
@@ -22,8 +28,13 @@ def main(option, method, thr, title):
     letter = cv2.waitKey(1) & 0xFF
     if letter == ord('w'):
       fall = False
-    if not ret or (letter == ord('q')):
+    if (letter == ord('q')):
       break
+
+    if t.time() - fall_time > 2000:
+      fall = False
+      fall_detected = False
+
     pose_points = pose.process(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)).pose_landmarks
     dt = t.time() - begin_time
     if pose_points:
@@ -40,9 +51,14 @@ def main(option, method, thr, title):
 
     if fall_detected or fall:
       display_text(frame,"Fall Detected!", (50, 50))
+      
+      if not fall:
+        fall_time = t.time()
+        
       fall = True
       #break
     begin_time = t.time()
+
     cv2.imshow(title, frame)
   #plt.plot(x,y)
   #plt.show()
@@ -51,4 +67,4 @@ def main(option, method, thr, title):
   return fall
 
 if __name__ == "__main__":
-  main(0, acceleration_thr, 100, "kill me")
+  main(1, acceleration_thr, 100, "Fall Detection Algorithm")
